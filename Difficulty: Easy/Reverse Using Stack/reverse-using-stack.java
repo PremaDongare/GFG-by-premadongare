54777d4ae6772251e954de578ef9dee3d708bb73
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-01-21 23:08:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Using Stack/reverse-using-stack.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            Solution obj = new Solution();
+            System.out.println(obj.reverse(sc.next()));
+        
+System.out.println("~");
+}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    public String reverse(String str){
+        //code here
+        Stack<Character> s = new Stack <>();
+        int idx = 0;
+        while(idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+
+}
