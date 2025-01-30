178d3ec134dfd455cd078a298e5206d7870077c3
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-01-30 17:59:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String st = sc.next();
+
+            char ans = new Solution().nonRepeatingChar(st);
+
+            if (ans != '$')
+                System.out.println(ans);
+            else
+                System.out.println(-1);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to find the first non-repeating character in a string.
+    static char nonRepeatingChar(String str) {
+        // Your code here
+        int freq[] = new int[26];
+        Queue<Character> q = new LinkedList<>();
+        
+        for (int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+            while(!q.isEmpty() && freq[q.peek()-'a']>1){
+                q.remove();
+            }
+        }
+        return q.isEmpty() ? '$' : q.peek();
+    }
+}
+
