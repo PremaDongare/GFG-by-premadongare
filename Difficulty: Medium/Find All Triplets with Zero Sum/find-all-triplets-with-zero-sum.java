c9a3b2435841b4fb31f1510a395a9bf3362f424d
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-06-17 14:34:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find All Triplets with Zero Sum/find-all-triplets-with-zero-sum.java
+++ Difficulty: Medium/Find All Triplets with Zero Sum/find-all-triplets-with-zero-sum.java
@@ -2,14 +2,18 @@
 class Solution {
     public List<List<Integer>> findTriplets(int[] arr) {
         int n = arr.length;
-        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        
+         List<List<Integer>> result = new ArrayList<>(set);
         
         for(int i=0; i<n-2; i++){
             for(int j=i+1;j<n-1;j++){
                 for(int k=j+1; k<n;k++){
                     if(arr[i]+arr[j]+arr[k]==0){
                         List<Integer>triplet=Arrays.asList(i, j, k);
-                        result.add(triplet);
+                        Collections.sort(triplet);
+                        set.add(triplet);
+                         result.add(triplet);
                     }
                 }
             }
