2fb37a96652eb6f2ca641478fe90291e15254682
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-07-01 14:15:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.java
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -1,67 +1,20 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine()); // Number of test cases
-        for (int g = 0; g < t; g++) {
-            String[] str =
-                (br.readLine()).trim().split(" "); // Reading input as a string array
-            int arr[] = new int[str.length]; // Creating integer array from the input
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-
-            // Getting the result from the Solution class
-            ArrayList<Integer> result = new Solution().nextLargerElement(arr);
-
-            // Printing the result in the required format
-            if (result.isEmpty()) {
-                System.out.println("[]");
-            } else {
-                for (int i = 0; i < result.size(); i++) {
-                    if (i != 0) System.out.print(" ");
-                    System.out.print(result.get(i));
-                }
-                System.out.println();
-                System.out.println("~");
-            }
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to find the next greater element for each element of the array.
     public ArrayList<Integer> nextLargerElement(int[] arr) {
         // code here
-        Stack<Integer> s = new Stack<>();
-        int nxGreater[] = new int[arr.length];
-        
-        for(int i=arr.length-1; i>=0; i--){
-            // 1st while 
-            while(!s.isEmpty() && arr[s.peek()] <= arr[i]){
-                s.pop();
-            }
-            //2 if-else
-            if(s.isEmpty()){
-                nxGreater[i] = -1;
-            }
-            else{
-                nxGreater[i] = arr[s.peek()];
-            }
-            // 3 push
-            s.push(i);
-        }
-        // Create an ArrayList to return the result.
+        int n= arr.length;
         ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i < nxGreater.length; i++) {
-            result.add(nxGreater[i]);
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=n-1; i>=0; i--){
+            // pop element smaller thn curr
+            while(!stack.isEmpty() && stack.peek()<= arr[i]){
+                stack.pop();
+            }
+            
+            // if stack is empty 
+           result.add(0, stack.isEmpty() ? -1 : stack.peek());
+            stack.push(arr[i]);
         }
-        return result; // Return the final list.
+        return result;
     }
 }
