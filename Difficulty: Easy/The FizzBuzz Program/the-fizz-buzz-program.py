052f92173309cb88032c984b2d4e84f25f1a01c6
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-07-05 15:51:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+def fizzBuzz(n):
+    # Write your code here.
+    if n % 3 == 0 and n % 5 == 0:
+         print ('FizzBuzz')
+    elif (n % 3 == 0):
+        print('Fizz')
+    elif (n % 5 == 0 ):
+        print ('Buzz')
+    else:
+        print(n)
+       
