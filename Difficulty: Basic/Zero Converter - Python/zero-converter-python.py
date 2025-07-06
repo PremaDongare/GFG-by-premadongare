58249753514b04cf66af744c861571605b347c18
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-07-06 13:49:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Zero Converter - Python/zero-converter-python.py
@@ -0,0 +1,15 @@
+#Back-end complete function Template for Python 3
+
+
+def pos(n):
+    n -= 1
+    while n >= 0:
+        print(n, end=' ')
+        n-=1
+        
+    
+def neg(n):
+    
+    while n <= 0:
+        print(n, end=' ')
+        n += 1
