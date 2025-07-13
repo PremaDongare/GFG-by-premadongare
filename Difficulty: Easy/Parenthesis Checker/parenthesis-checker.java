3bd430f723c0c7831ae7760fa28e1e5500cc62c4
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-07-13 16:25:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,63 +1,25 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Driverclass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        // Reading total number of testcases
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            // reading the string
-            String st = sc.next();
-
-            // calling ispar method of Paranthesis class
-            // and printing "balanced" if it returns true
-            // else printing "not balanced"
-            if (new Solution().isParenthesisBalanced(st) == true)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 
 class Solution {
-    // Function to check if brackets are balanced or not.
-    static boolean isParenthesisBalanced(String s) {
-        Stack<Character> st = new Stack<>();
+    static boolean isBalanced(String s) {
+        // code here
+        Stack<Character> st = new Stack <>();
         for(int i=0; i<s.length();i++){
-            char ch= s.charAt(i);
+            char c = s.charAt(i);
             
-            if(ch =='(' || ch == '{' || ch == '['){
-                st.push(ch);
+            if(c=='(' || c == '[' || c=='{'){
+                st.push(c);
             }else{
-                if(st.isEmpty()){
-                    return false;
-                }
-                if((st.peek() == '(' && ch == ')')
-                ||(st.peek() == '{' && ch == '}')
-                ||(st.peek() == '[' && ch == ']')){
-                    st.pop();
-                }
-                else{
-                    return false;
-                }
+                if (st.isEmpty()) return false;
+                
+                char top =st.pop();
+                if((c==')'  && top != '(' )||
+                    (c==']' && top != '[' )||
+                    (c=='}' && top != '{')){
+                        return false;
+                    }
             }
         }
-        if(st.isEmpty()){
-            return true;
-        }else{
-            return false;
-        }
         
+        return st.isEmpty();
     }
 }
