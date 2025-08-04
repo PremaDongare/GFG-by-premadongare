be4b6a43e6af4f85a1a81e39420659c0aa6cc296
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-08-04 10:00:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print Anagrams Together/print-anagrams-together.java
+++ Difficulty: Medium/Print Anagrams Together/print-anagrams-together.java
@@ -1,25 +1,37 @@
 class Solution {
-    public ArrayList<ArrayList<String>> anagrams(String[] arr) {
-        Map<String, List<String>> map = new HashMap<>();
-
-        for (String str : arr) {
-            char[] charArray = str.toCharArray();
-            Arrays.sort(charArray);
-            String sortedStr = new String(charArray);
-
-            if (!map.containsKey(sortedStr)) {
-                map.put(sortedStr, new ArrayList<>());
+    
+        // code here
+        private String generate(String s) {
+        int [] count = new int[26];
+        
+        for(char ch : s.toCharArray()){
+            count[ch -'a']++;
+        }
+        
+        // key writning process
+        StringBuilder news = new StringBuilder();
+        for(int i=0; i<26; i++){
+            if(count[i]>0){
+                news.append(String.valueOf((char) (i+'a')).repeat(count[i]));
             }
-
-            map.get(sortedStr).add(str);
         }
-
-        // Now convert map.values() into ArrayList<ArrayList<String>>
-        ArrayList<ArrayList<String>> result = new ArrayList<>();
-        for (List<String> group : map.values()) {
-            result.add(new ArrayList<>(group)); 
-        }
-
-        return result;
+        
+        return news.toString();
+        
     }
+    public ArrayList<ArrayList<String>> anagrams(String[] strs) {
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        
+        for(String s:strs){
+            String news = generate(s);
+            
+            if(!map.containsKey(news)){
+                map.put(news,new ArrayList<>());
+            }
+            
+            map.get(news).add(s);
+        }
+        
+    return new ArrayList<>(map.values());
+}
 }
