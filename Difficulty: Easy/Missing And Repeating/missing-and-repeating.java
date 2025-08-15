d3727886bd5daf9fa8763fc518ca485ddd7be4e8
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-08-15 14:22:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -1,56 +1,24 @@
-//{ Driver Code Starts
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList; // Import ArrayList
-import java.util.Stack;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String input = br.readLine();
-            String[] inputs = input.split(" ");
-            int[] arr = new int[inputs.length];
-
-            for (int i = 0; i < inputs.length; i++) {
-                arr[i] = Integer.parseInt(inputs[i]);
-            }
-
-            // Get the result as an ArrayList<Integer>
-            ArrayList<Integer> ans = new Solution().findTwoElement(arr);
-            System.out.println(ans.get(0) + " " + ans.get(1));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // Function to find two elements in array
     ArrayList<Integer> findTwoElement(int arr[]) {
-        int n = arr.length;
-        int [] frequency= new int [n+1];
-        ArrayList<Integer> result = new ArrayList<>();
-        
-        for (int i=0; i<n; i++){
-            frequency[arr[i]]++;
-        }
-        int rep = -1, miss = -1;
-        for(int i=1; i<=n; i++){
-            if(frequency[i]==0) miss =i;
-            if(frequency[i] == 2)rep =i;
-        }
-        result.add(rep);
-        result.add(miss);
-        return result;
         // code here
+        int n = arr.length;
+        int [] hash = new int [n+1];
+        
+        for(int i=0; i<n; i++){
+            hash[arr[i]]++;
+        }
+        
+        int repet = -1, missing=-1;
+        for(int i=1; i<=n;i++){
+            if(hash[i] == 2) repet = i;
+            else if(hash[i] ==0) missing = i;
+            
+            if(repet !=-1 && missing!= -1)
+            break;
+        }
+       ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(repet);
+        ans.add(missing);
+        return ans;
     }
 }
