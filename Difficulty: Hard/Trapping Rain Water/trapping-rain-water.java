73effe0915a14558c633d14260ba8cd40086cbb1
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2026-03-02 22:48:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxWater(int height[]) {
+        // code here
+        int l = 0;
+        int r=height.length-1;
+        int maxl=0, maxr=0;
+        int water=0;
+        
+        while(l<r){
+            if(height[l]<height[r]){
+                if(height[l]>= maxl){
+                    maxl=height[l];
+                }else{
+                    water+= maxl-height[l];
+                }l++;
+            }else{
+                if(height[r]>= maxr){
+                    maxr = height[r];
+                }else{
+                    water += maxr-height[r];
+                }
+                r--;
+            }
+        }
+        return water;
+    }
+}
